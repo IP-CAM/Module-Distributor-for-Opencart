3389b8e5676b154a2fea2b2a2d5ad8d349e69708
denis <denis.kisel92@gmail.com>
2018-06-12 16:32:16 +0300

    run replace foreach

--- run.php
+++ run.php
@@ -8,10 +8,10 @@ $config = require_once __DIR__ . '/config.php';
 $structureAndConformity = require_once __DIR__ . '/rules/structureAndConformity.php';
 $filesToDistribute = require_once __DIR__ . '/filesToDistribute.php';
 
-foreach ($filesToDistribute as $adminCatalogDirName => $adminCatalogDirs) {
-    foreach ($adminCatalogDirs as $mvcDirName => $files) {
-        foreach ($files as $file) {
-            foreach ($config['integration_versions'] as $integrationVersion) {
+foreach ($config['integration_versions'] as $integrationVersion) {
+    foreach ($filesToDistribute as $adminCatalogDirName => $adminCatalogDirs) {
+        foreach ($adminCatalogDirs as $mvcDirName => $files) {
+            foreach ($files as $file) {
                 $structureDirFileToCopy = $structureAndConformity[$structureAndConformity['conformity'][$config['distribution_version']]][$adminCatalogDirName][$mvcDirName];
                 $fileToCopy = FileSystem::parentDir() . $config['distribution_version'] . '/' . $structureDirFileToCopy . $file;
 
