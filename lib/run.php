fda28f9e1325d4dbda096bad46be93b6d11a87af
Denis Kisel <denis.kisel92@gmail.com>
2021-03-23 10:07:32 +0000

    mk exception\

--- lib/run.php
+++ lib/run.php
@@ -2,4 +2,8 @@
 $loader = require __DIR__ . '/vendor/autoload.php';
 use App\Controller;
 
+if (!file_exists('distributor_config.php') || !is_dir('d_rules')) {
+    throw new Exception('Not found config files. You can create this by command - [oc-dist-deploy] ');
+}
+
 Controller::run();
