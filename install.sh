316b956583e0628c19dac87ee59d53d9735d0e36
Denis Kisel <denis.kisel92@gmail.com>
2021-03-23 09:57:25 +0000

    ref structure

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp bin/* /usr/local/bin
+
+mkdir -p /usr/local/lib/oc-distributor
+cp -R lib/* /usr/local/lib/oc-distributor
+
+chmod 755 /usr/local/bin/oc-distribute
+chmod 755 /usr/local/bin/oc-dist-deploy
+chmod -R 755 /usr/local/lib/oc-distributor
---
+++ lib/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /usr/local/lib/oc-distributor/default.conf
+
+cp $LIB_PATH/distributor_config.php.stub distributor_config.php
+mkdir -p d_rules
+cp $LIB_PATH/d_rules/* d_rules/
