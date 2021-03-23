573e981d8002e67ae2a74f008401e525347f30da
Denis Kisel <denis.kisel92@gmail.com>
2021-03-23 10:19:55 +0000

    fix install

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 cp bin/* /usr/local/bin
 
+rm -R /usr/local/lib/oc-distributor
 mkdir -p /usr/local/lib/oc-distributor
 cp -R lib/* /usr/local/lib/oc-distributor
 
 chmod 755 /usr/local/bin/oc-distribute
 chmod 755 /usr/local/bin/oc-dist-deploy
 chmod -R 755 /usr/local/lib/oc-distributor
+
+echo 'Success!'
